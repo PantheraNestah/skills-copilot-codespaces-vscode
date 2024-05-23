0792b3ac161b8388bc54b6788120eae287e480ed
Gift Nestah <94760907+PantheraNestah@users.noreply.github.com>
2024-05-23 17:35:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMembers() {
+    var members = [
+        { name: 'John', age: 20, skills: ['JavaScript', 'React'] },
+        { name: 'Albert', age: 25, skills: ['JavaScript', 'Angular'] },
+        { name: 'Helen', age: 30, skills: ['HTML', 'CSS'] }
+    ];
+
+    return members;
+}
